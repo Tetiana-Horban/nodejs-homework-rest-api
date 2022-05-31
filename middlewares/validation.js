034bc4e519c596d0589aaf1bbec9b28d0240f29b
Tetiana-Horban <tanya.ryptyk@gmail.com>
2022-05-31 21:53:17 +0300

    corrected

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,6 +1,6 @@
-const validation = (shema) => {
+const validation = (schema) => {
   return (req, res, next) => {
-    const { error } = shema.validate(req.body);
+    const { error } = schema.validate(req.body);
     if (error) {
       error.status = 400;
       next(error);
