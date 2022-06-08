27ee3250a44499d1235e09c5fd937a0955e90cb9
Tetiana-Horban <tanya.ryptyk@gmail.com>
2022-06-08 20:39:55 +0300

    Create signup.js

---
+++ controllers/users/signup.js
@@ -0,0 +1,24 @@
+const { User } = require("../../models");
+const { Conflict } = require("http-errors");
+
+const signup = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw new Conflict(`User with ${email} already exist`);
+  }
+  const newUser = new User({ email });
+  newUser.setPassword(password);
+  newUser.save();
+  res.status(201).json({
+    status: "success",
+    code: 201,
+    data: {
+      user: {
+        email,
+      },
+    },
+  });
+};
+
+module.exports = signup;
