dc714e86b8e02cfe77502d97639c3026f9b1fc34
Tetiana-Horban <tanya.ryptyk@gmail.com>
2022-06-18 15:25:01 +0300

    add updateAvatar

--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -1,5 +1,6 @@
 const { User } = require("../../models");
 const { Conflict } = require("http-errors");
+const gravatar = require("gravatar");
 
 const signup = async (req, res) => {
   const { email, password } = req.body;
@@ -7,7 +8,8 @@ const signup = async (req, res) => {
   if (user) {
     throw new Conflict(`User with ${email} already exist`);
   }
-  const newUser = new User({ email });
+  const avatarURL = gravatar.url(email);
+  const newUser = new User({ email, avatarURL });
   newUser.setPassword(password);
   newUser.save();
   res.status(201).json({
@@ -16,6 +18,7 @@ const signup = async (req, res) => {
     data: {
       user: {
         email,
+        avatarURL,
       },
     },
   });
